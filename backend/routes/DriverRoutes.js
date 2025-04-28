781c697f9f9e24aa8de4d127edc4c9f53e5254f2
Shali29 <shalikaramanayaka21@gmail.com>
2025-04-28 11:11:18 +0530

    call all of the driver controllers paths to driver routes

--- backend/routes/DriverRoutes.js
+++ backend/routes/DriverRoutes.js
@@ -1,21 +1,25 @@
 import express from 'express';
-import driverController from '../controllers/DriverController.js';
+import {getAllDrivers} from '../controllers/DriverController.js';
+import {getDriverById} from '../controllers/DriverController.js';
+import {createDriver} from '../controllers/DriverController.js';
+import { updateDriver } from '../controllers/DriverController.js';
+import { deleteDriver } from '../controllers/DriverController.js';
 
 const router = express.Router();
 
 // GET all drivers
-router.get('/', driverController.getAllDrivers);
+router.get('/', getAllDrivers);
 
 // GET driver by ID
-router.get('/:id', driverController.getDriverById);
+router.get('/:id', getDriverById);
 
 // POST create new driver
-router.post('/', driverController.createDriver);
+router.post('/', createDriver);
 
 // PUT update driver
-router.put('/:id', driverController.updateDriver);
+router.put('/:id', updateDriver);
 
 // DELETE driver
-router.delete('/:id', driverController.deleteDriver);
+router.delete('/:id', deleteDriver);
 
 export default router;
