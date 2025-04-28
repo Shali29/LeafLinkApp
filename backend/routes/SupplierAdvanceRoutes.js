1691df6d22e761a87f62471f1eeadf219d75d1be
Shali29 <shalikaramanayaka21@gmail.com>
2025-04-28 10:57:17 +0530

    call all of the supplier advance collections tosupplier advance routes

--- backend/routes/SupplierAdvanceRoutes.js
+++ backend/routes/SupplierAdvanceRoutes.js
@@ -1,30 +1,36 @@
 import express from 'express';
-import supplierAdvanceController from '../controllers/supplierAdvanceController.js';
-
+import {getAllAdvances} from '../controllers/SupplierAdvanceController.js';
+import {getAdvancesBySupplier} from '../controllers/SupplierAdvanceController.js';
+import {getAdvanceById} from '../controllers/SupplierAdvanceController.js';
+import { createAdvance } from '../controllers/SupplierAdvanceController.js';
+import { updateAdvance } from '../controllers/SupplierAdvanceController.js';
+import { deleteAdvance } from '../controllers/SupplierAdvanceController.js';
+import { updateAdvanceStatus } from '../controllers/SupplierAdvanceController.js';
+import { getAdvanceStatistics } from '../controllers/SupplierAdvanceController.js';
 const router = express.Router();
 
 // GET all advances
-router.get('/', supplierAdvanceController.getAllAdvances);
+router.get('/', getAllAdvances);
 
 // GET advances by supplier ID
-router.get('/supplier/:supplierId', supplierAdvanceController.getAdvancesBySupplier);
+router.get('/supplier/:supplierId', getAdvancesBySupplier);
 
 // GET advance by ID
-router.get('/:id', supplierAdvanceController.getAdvanceById);
+router.get('/:id', getAdvanceById);
 
 // POST create new advance
-router.post('/', supplierAdvanceController.createAdvance);
+router.post('/', createAdvance);
 
 // PUT update advance
-router.put('/:id', supplierAdvanceController.updateAdvance);
+router.put('/:id', updateAdvance);
 
 // DELETE advance
-router.delete('/:id', supplierAdvanceController.deleteAdvance);
+router.delete('/:id', deleteAdvance);
 
 // PUT update advance status
-router.put('/:id/status', supplierAdvanceController.updateAdvanceStatus);
+router.put('/:id/status', updateAdvanceStatus);
 
 // GET advance statistics
-router.get('/stats/summary', supplierAdvanceController.getAdvanceStatistics);
+router.get('/stats/summary', getAdvanceStatistics);
 
 export default router;
