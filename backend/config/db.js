6171f94d2ee90e9f2f4b26a81c97ae32de7932dd
Shali29 <shalikaramanayaka21@gmail.com>
2025-04-28 15:41:43 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,11 @@
 import mysql from 'mysql2/promise';
 
-const dbURI = 'mysql://root:abcd1234@127.0.0.1:3306/leaflinkapp_db';
-
-const pool = mysql.createPool(dbURI);
-
-// Define a testConnection function
-export async function testConnection() {
-    try {
-        const connection = await pool.getConnection();
-        console.log('Database connected successfully!');
-        connection.release();
-    } catch (error) {
-        console.error('Database connection failed:', error);
-    }
-}
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'abcd1234', 
+    database: 'leaflinkapp_db' 
+});
 
 export default pool;
